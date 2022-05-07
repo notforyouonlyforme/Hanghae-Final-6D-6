e8f51b50a2e8576771482369bd63175ad120952e
ggongdori <soominlee0410@gmail.com>
2022-05-07 20:30:23 +0900

    comment update initial

---
+++ src/main/java/com/example/sparta/hanghaefinal/websocket/WebSockChatHandler.java
@@ -0,0 +1,7 @@
+package com.example.sparta.hanghaefinal.websocket;
+
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+public class WebSockChatHandler extends TextWebSocketHandler {
+
+}
